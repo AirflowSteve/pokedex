6ef7d5a637d7836eea9f1df45c91c5d8c45742e7
AirflowSteve <nlifanov02@gmail.com>
2026-09-13 20:29:15 +0300

    Added 'help' and 'exit' functions to the REPL

--- main.go
+++ main.go
@@ -1,26 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		input := ""
-		if scanner.Scan() {
-			input = scanner.Text()
-		} else {
-			fmt.Errorf("uh-uh, didn't get it\n")
-		}
-		clean_input := cleanInput(input)
-		if len(clean_input) != 0 {
-			fmt.Printf("Your command was: %s\n", clean_input[0])
-		} else {
-			fmt.Println("Don't ignore me, please")
-		}
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,10 +1,77 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+type cliCommands struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
+		commandName := words[0]
+
+		command, ok := getCommands()[commandName]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		} else {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
 	return words
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("")
+	for cmdName, value := range getCommands() {
+		fmt.Printf("%s: %s\n", cmdName, value.description)
+	}
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func getCommands() map[string]cliCommands {
+	return map[string]cliCommands{
+		"exit": {
+			name:        "exit",
+			description: "Exit the pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
